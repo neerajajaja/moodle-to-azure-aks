6f128a0f840782848f289d029472a2cf9f1b4fea
neerajajaja <jneeraja@iitg.ac.in>
2021-06-14 16:05:09 +0530

    updated https in config

--- scripts/install_moodle.sh
+++ scripts/install_moodle.sh
@@ -199,7 +199,7 @@ conf_to_replace="if (empty(\$_SERVER['HTTP_HOST'])) {\\
   \$_SERVER['HTTP_HOST'] = '127.0.0.1:${http_port}';\\
 }\\
 if (isset(\$_SERVER['HTTPS']) \&\& \$_SERVER['HTTPS'] == 'on') {\\
-  \$CFG->wwwroot   = 'https\/\/:' . \$_SERVER['HTTP_HOST'];\\
+  \$CFG->wwwroot   = 'https:\/\/' . \$_SERVER['HTTP_HOST'];\\
 } else {\\
   \$CFG->wwwroot   = 'http:\/\/' . \$_SERVER['HTTP_HOST'];\\
 }"
